faec906609de3408cfd619ac89401e1ed4fd43bd
grantbradshaw <grant.bradshaw@outlook.com>
2018-08-06 09:44:46 -0400

    Renaming main file, deployment executable

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
+# from flask.ext.sqlalchemy import SQLAlchemy
 # from models.result import Result
 # import environment
 import os
