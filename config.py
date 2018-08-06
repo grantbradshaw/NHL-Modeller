d2e2a66db28bea6f481285e18fb414463de9c75a
grantbradshaw <grant.bradshaw@outlook.com>
2018-08-06 09:18:23 -0400

    Initializing deployment config

---
+++ config.py
@@ -0,0 +1,24 @@
+import os
+import environment
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = environment.SECRET_KEY
+    SQLALCHEMY_DATABASE_URI = environment.DATABASE_URL
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+class TestingConfig(Config):
+    TESTING = True
