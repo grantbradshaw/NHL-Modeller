ffd1fcc0d2f4b87c5f906787086c61b0f065e120
grantbradshaw <grant.bradshaw@outlook.com>
2018-08-07 09:30:25 -0400

    Properly setting environment variables.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask
 import os
 
 app = Flask(__name__)
-# app.config.from_object(environment.DATABASE_URL)
+app.config.from_object(os.environ['APP_SETTINGS'])
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # db = SQLAlchemy(app)
 
