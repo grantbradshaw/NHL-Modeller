8db507c63119aa5e9a1ba10bccdc4fa50734f979
grantbradshaw <grant.bradshaw@outlook.com>
2018-08-09 14:24:56 -0400

    Scraping and saving contract summary information

--- scripts/scrape_contracts.py
+++ scripts/scrape_contracts.py
@@ -65,13 +65,19 @@ def scrape_player_data(url):
 
         # gets personal information at top of page
         for div in html.findAll('div', {'class': 'indx_b'}):
-            find_deepest(div, url)
+            find_deepest_personal(div, url)
+
+        # get contract information (assumes url already exists in output)
+        i = 0 # represents index of contract in output[url][contracts]
+        for div in html.findAll('div', {'class': 'table_c'}):
+            find_contract_info(div, url, i)
+            i+= 1
 
 # method to get deepest nested divs
-def find_deepest(beautiful_html, url):
+def find_deepest_personal(beautiful_html, url):
     if len(beautiful_html.findAll('div')):
         for div in beautiful_html.findChildren('div', recursive=False):
-            find_deepest(div, url)
+            find_deepest_personal(div, url)
     else:
         process_personal(beautiful_html, url)
 
@@ -87,16 +93,60 @@ def process_personal(html, url, key=None):
         value = data[1].replace('>' , '').replace('<', '').replace(':', '').strip()
         add_to_output(key, value, url)
 
+# method to find contract tables and pass divs without contract tables
+def find_contract_info(div, url, i):
+    # ignore non-contract table divs, such as salary progression charts
+    if len(div.findAll('div', {'class': 'cntrct'})):
+        find_deepest_contract_summary(div.find('div', {'class': 'column_head3 rel cntrct'}), url, i)
+
+# method to find lowest level divs in contract summary section
+def find_deepest_contract_summary(beautiful_html, url, i):
+    if len(beautiful_html.findAll('div')):
+        for div in beautiful_html.findChildren('div', recursive=False):
+            find_deepest_contract_summary(div, url, i)
+    else:
+        process_contract_summary(beautiful_html, url, i)
+
+# method to process contract summary information
+def process_contract_summary(div, url, i):
+    contract = {}
+    string = str(div)
+    # not interested in source, not saving to output
+    if 'SOURCE' not in string:
+        data = re.findall(">[A-Za-z0-9',\(\):\ ]+<", string)
+        if len(data) == 1:
+            piece = data[0].replace('>' , '').replace('<', '')
+            key_value = piece.split(':')
+            if len(key_value) == 2:
+                key = key_value[0].title().strip()
+                value = key_value[1].title().strip()
+                add_to_contracts(key, value, url, i)
+            else:
+                raise Exception('Contract section formatted inconsistent with others')
+        elif len(data) > 1:
+            raise Exception('Page formatted inconsistent with others')
+
 # simple method to add key value pair to output
 def add_to_output(key, value, url):
     if url in output:
         output[url][key] = value
     else:
-        output[url] = {key: value}
+        output[url] = {key: value,
+                       'contracts': [],
+                       'contract year': []}
+
+# method to add to a contract
+def add_to_contracts(key, value, url, i):
+    if len(output[url]['contracts']) <= i:
+        output[url]['contracts'].append({
+                                       key: value
+                                       })
+    else:
+        output[url]['contracts'][i][key] = value
 
 # get_contract_index_pages()
 scrape_player_data('https://www.capfriendly.com/players/connor-mcdavid')
-# print(output)
+pp.pprint(output)
 
 
 
