59320769edf109ada5034357e94d5296864c60d8
grantbradshaw <grant.bradshaw@outlook.com>
2018-08-25 19:54:42 -0400

    Renaming column in season table'

--- models/season.py
+++ models/season.py
@@ -5,17 +5,17 @@ class Season(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     start_date = db.Column(db.Date)
-    end_regular_season = db.Column(db.Date)
+    start_playoffs = db.Column(db.Date)
     end_playoffs = db.Column(db.Date)
     cap_floor = db.Column(db.Float)
     cap_ceiling = db.Column(db.Float)
     games = db.Column(db.Float)
     contract_seasons = db.relationship('ContractSeason', backref='seasons', lazy=True)
 
-    def __init__(self, start_date, end_regular_season, end_playoffs, cap_floor,
+    def __init__(self, start_date, start_playoffs, end_playoffs, cap_floor,
                  cap_ceiling, games, contracts, contract_seasons, draft_picks):
         self.start_date = start_date
-        self.end_regular_season = end_regular_season
+        self.start_playoffs = start_playoffs
         self.end_playoffs = end_playoffs
         self.cap_floor = cap_floor
         self.cap_ceiling = cap_ceiling
